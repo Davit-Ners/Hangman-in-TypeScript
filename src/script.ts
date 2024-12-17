254a63560f8e0caa5d248972cf376d72c518ac01
Davit <davit.ners02@gmail.com>
2024-12-17 13:52:53 +0100

    Finalisation de la logique algorithmique du jeu

--- src/script.ts
+++ src/script.ts
@@ -33,34 +33,49 @@ function placerLettres(): void {
 }
 
 function getMot(): string {
-    return 'comme';
+    return 'commerrrage';
 }
 
-function getTirets() : string[] {
-    const tableau : string[] = [];
+// Fonction pour avoir un tableau de tirets pour le mot caché a afficher
+function getTirets(): string[] {
+    const tableau: string[] = [];
     for (let lettre of tabMot) {
         tableau.push('_');
     }
     return tableau;
 }
 
+// Fonction principale du jeu
 function game(btn: HTMLButtonElement): void {
     let lettre: string = btn.textContent!;
     if (tabMot.includes(lettre)) {
+        let found = 0;
         while (tabMot.indexOf(lettre) != -1) {
+            console.log(found);
+            const index = motCache.indexOf(lettre, found);
+            found = index;
+            tabCache[index] = lettre;
             tabMot.splice(tabMot.indexOf(lettre), 1);
             console.log(tabMot);
+            found++;
         }
-        btn.removeEventListener('click', function () {game(btn)});
+        btn.removeEventListener('click', function () { game(btn) });
         btn.remove();
+        zoneMotCache.textContent = tabCache.join(' ');
     }
     else {
         chance--;
         chanceRestantesP.textContent = chance.toString();
-        btn.removeEventListener('click', function () {game(btn)});
+        btn.removeEventListener('click', function () { game(btn) });
         btn.remove();
+    }
+    if (chance == 0) {
+        alert('perdu');
+    }
+    if (tabMot.length == 0) {
+        alert('gagné');
     }
 }
 
 placerLettres();
-zoneMotCache.textContent = getTirets().join(' ');
+zoneMotCache.textContent = tabCache.join(' ');
